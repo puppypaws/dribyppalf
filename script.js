a3738a34c8f1372be79bb1ac175c5e8662898427
Romeo <Romeo@Romeos-MacBook-Air.local>
2019-03-27 23:59:27 +0800

    Added scoring table, browser compatibility, optimised layout

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ var bg = new Image();
 var fg = new Image();
 var northBlock = new Image();
 var southBlock = new Image();
-red.src = 'pics/red.gif'; //50x50
+red.src = 'pics/redv2.png'; //29x33
 bg.src = 'pics/cyanbg.png'; //288x512
 fg.src = 'pics/greenfg.png'; //306x118
 northBlock.src = 'pics/northBlock.png '; //52x242
@@ -16,13 +16,15 @@ southBlock.src = 'pics/southBlock.png'; //52x378
 
 //***OBJECT PLACEMENT***//
 //Gap between pillars
-var gap = 85;
+var gap = 73;
 var constant;
 //Player start position (starts from top left) (y-axis is inverted +/-)
 var bX = 30;
 var bY = 150;
 //Push square down, greater = stronger
 var gravity = 1.5;
+//Score variable
+var score = 0;
 //Block co-od
 var block = [];
 //Restrict blocks to screen dimensions
@@ -32,11 +34,60 @@ block[0] = {
 };
 
 //***MOVEMENT***//
-//Move up on keypress
-document.addEventListener('keydown',moveUp);
-function moveUp (){
-    bY -= 20; //Distance moved per keypress
+//Small hop
+function smallHopPress(event) {
+  var x = event.keyCode || event.which;
+    console.log(x + " was pressed");
+        if (x === 87) {
+        smallHop();
+        }
 }
+document.addEventListener('keydown',smallHopPress);
+function smallHop (){
+    bY -= 25; //Distance moved per keypress
+}
+
+//Big hop
+function bigHopPress(event) {
+  var x = event.keyCode || event.which;
+    console.log(x + " was pressed");
+        if (x === 32) { //Spacebar
+        bigHop();
+        }
+}
+document.addEventListener('keydown',bigHopPress);
+function bigHop (){
+    bY -= 35; //Distance moved per keypress
+}
+
+//Kamikaze
+function kamikazePress(event) {
+  var x = event.keyCode || event.which;
+    console.log(x + " was pressed");
+        if (x === 68) { //D
+        kamikaze();
+        }
+}
+document.addEventListener('keydown',kamikazePress);
+function kamikaze (){
+    bX += 20;
+    bY -= 5; //Distance moved per keypress
+}
+
+//Abort Mission
+function abortMissionPress(event) {
+  var x = event.keyCode || event.which;
+    console.log(x + " was pressed");
+        if (x === 65) { //A
+        abortMission();
+        }
+}
+document.addEventListener('keydown',abortMissionPress);
+function abortMission (){
+    bX -= 20;
+    bY -= 5; //Distance moved per keypress
+}
+
 
 //***GAME LOGIC***//
 //Draw Map & blocks | bg:background | cvs:canvas | fg:foreground
@@ -57,6 +108,10 @@ function draw(){
             y : Math.floor(Math.random()*northBlock.height)-northBlock.height //Randomise vertical gap
             });
 }
+//***SCORING***//
+if( block[i].x == 5) { //If block cood: x=5, add +1 to score
+    score++;
+}
 //***COLLISION DETECTION***//
 if( bX + red.width >= block[i].x               //Player x-pos+width  Greater  block x-pos
     &&                                         //AND
@@ -67,23 +122,31 @@ if( bX + red.width >= block[i].x               //Player x-pos+width  Greater  bl
     bY + red.height >= block[i].y+constant)    //Player y-pos+height Greater  block y-pos+(height+gap)
     ||                                         //OR
     bY + red.height >= cvs.height - fg.height) //Player y-pos+height Greater  canvas height - foreground height
-
-{location.reload();} //If locations match, reload game
+{
+//***RESET***//
+location.reload();} //If locations match, reload game
 }
 
-//***DRAW PLAYER & FG***//
+//***DRAW PLAYER,FG & SCORE***//
 //(Object,x-pos,y-pos using canvas as reference)
 obj.drawImage(fg,0,cvs.height-fg.height);
 //(object,x-pos,y-pos predetermined on lines 22 & 23)
 obj.drawImage(red,bX,bY);
-//Apply gravity to player's bY
+//Score format & location
+obj.fillStyle = '#000';
+obj.font = "20px Roboto";
+obj.fillText("Score : "+score,10,cvs.height-20);
+
+//Apply gravity to player's bY *Must be placed below logic*
 bY += gravity;
+
 //Infinite drawing loop begins
 requestAnimationFrame(draw);
 }
 draw();
 
 //***TO-DO***//
-//Sound, (hi)score, graphics
-//Extra:       Start button, difficulty level
-//Extra extra: Online leaderboard
+//Add more block variety: wider which requires new button (go forward)
+//Sound, hi-score, graphics
+//Start button, difficulty level
+//Online leaderboard
